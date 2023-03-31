94d17d919d7909b2ba7df8768dfdb373d599de49
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-31 12:49:53 -0300

    feat: cria a classe ProductController e adiciona a funcao para criar um novo produto

---
+++ src/controllers/product.controller.ts
@@ -0,0 +1,14 @@
+import { Request, Response } from 'express';
+import ProductService from '../services/product.service';
+
+class ProductController {
+  constructor(private productService = new ProductService()) {}
+
+  public createProduct = async (req: Request, res: Response) => {
+    const product = req.body;
+    const productCreated = await this.productService.createProduct(product);
+    res.status(200).json(productCreated);
+  };
+}
+
+export default ProductController;
