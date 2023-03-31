753354e3d1d5713c024e93d5cbb7f3446b00ffd2
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-31 16:13:04 -0300

    feat: adiciona a funcao getAllProducts na classe ProductController

--- src/controllers/product.controller.ts
+++ src/controllers/product.controller.ts
@@ -4,10 +4,15 @@ import ProductService from '../services/product.service';
 class ProductController {
   constructor(private productService = new ProductService()) {}
 
+  public getAllProducts = async (_req: Request, res: Response) => {
+    const allProducts = await this.productService.getAllProducts();
+    return res.status(200).json(allProducts);
+  };
+
   public createProduct = async (req: Request, res: Response) => {
     const product = req.body;
     const productCreated = await this.productService.createProduct(product);
-    res.status(201).json(productCreated);
+    return res.status(201).json(productCreated);
   };
 }
 
