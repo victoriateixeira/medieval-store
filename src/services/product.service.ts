717dc627e034ceb1d32fba5db33753ca2aa1ee50
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-03-31 21:31:47 -0300

    feat: cria a classe UserService e adiciona funcoes para criar e verficar token e funcao para criar novo usuario

--- src/services/product.service.ts
+++ src/services/product.service.ts
@@ -2,7 +2,7 @@ import connection from '../models/connection';
 import ProductModel from '../models/product.model';
 import { IProduct } from '../interfaces';
 
-const productProperties = ['name', 'amount'];
+// const productProperties = ['name', 'amount'];
 class ProductService {
   public model: ProductModel;
 
@@ -10,39 +10,39 @@ class ProductService {
     this.model = new ProductModel(connection);
   }
 
-  static validateProperties(product: IProduct): [boolean, string | null] {
-    for (let i = 0; i < productProperties.length; i += 1) {
-      if (!Object.prototype.hasOwnProperty.call(product, productProperties[i])) {
-        return [false, productProperties[i]];
-      }
-    }
-    return [true, null];
-  }
+  // static validateProperties(product: IProduct): [boolean, string | null] {
+  //   for (let i = 0; i < productProperties.length; i += 1) {
+  //     if (!Object.prototype.hasOwnProperty.call(product, productProperties[i])) {
+  //       return [false, productProperties[i]];
+  //     }
+  //   }
+  //   return [true, null];
+  // }
   
-  static validateValues(product: IProduct): [boolean, string | null] {
-    const entries = Object.entries(product);
-    for (let i = 0; i < entries.length; i += 1) {
-      const [property, value] = entries[i];
-      if (!value) {
-        return [false, property];
-      }
-    }
-    return [true, null];
-  }
+  // static validateValues(product: IProduct): [boolean, string | null] {
+  //   const entries = Object.entries(product);
+  //   for (let i = 0; i < entries.length; i += 1) {
+  //     const [property, value] = entries[i];
+  //     if (!value) {
+  //       return [false, property];
+  //     }
+  //   }
+  //   return [true, null];
+  // }
   
-  static validationBook(product: IProduct): void | string {
-    let [valid, property] = ProductService.validateProperties(product);
+  // static validationProduct(product: IProduct): void | string {
+  //   let [valid, property] = ProductService.validateProperties(product);
   
-    if (!valid) {
-      return `O campo ${property} é obrigatório.`;
-    }
+  //   if (!valid) {
+  //     return `O campo ${property} é obrigatório.`;
+  //   }
 
-    [valid, property] = ProductService.validateValues(product);
+  //   [valid, property] = ProductService.validateValues(product);
   
-    if (!valid) {
-      return `O campo ${property} não pode ser nulo ou vazio.`;
-    }
-  }
+  //   if (!valid) {
+  //     return `O campo ${property} não pode ser nulo ou vazio.`;
+  //   }
+  // }
   
   public async getAllProducts():Promise<IProduct[]> {
     const allProducts = this.model.getAllProducts();
---
+++ src/services/user.service.ts
@@ -0,0 +1,41 @@
+import jwt, { SignOptions } from 'jsonwebtoken';
+import connection from '../models/connection';
+import UserModel from '../models/user.model';
+import { IUser } from '../interfaces';
+
+const secret = process.env.JWT_SECRET || 'SecretFiller';
+
+const JWT_CONFIG: SignOptions = {
+  algorithm: 'HS256',
+  expiresIn: '7d',
+};
+
+class UserService {
+  public model: UserModel;
+
+  constructor() {
+    this.model = new UserModel(connection);
+  }
+
+  static createToken = (data: IUser) => jwt.sign(data, secret, JWT_CONFIG);
+
+  // const verifyToken = (token) => jwt.verify(token, secret);
+
+  static verifyToken = (authorization :string) => {
+    try {
+      const payload = jwt.verify(authorization, secret);
+      return payload;
+    } catch (error) {
+      throw new Error();
+    }
+  };
+
+  public async createUser(userData: IUser): Promise<string> {
+    const newUser = await this.model.createUser(userData);
+    const { password: _, ...userWithoutPassword } = newUser;
+    const token = UserService.createToken(userWithoutPassword);
+    return token;
+  }
+}
+
+export default UserService;
