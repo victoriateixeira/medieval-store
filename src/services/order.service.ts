7f794f1dd35fd05dff61195538de08a951c2f7c3
Victoria Teixeira <victoriafteixeira@hotmail.com>
2023-04-01 11:02:26 -0300

    feat: cria a classe OrderService a adiciona funcao para listar todos os pedidos

---
+++ src/services/order.service.ts
@@ -0,0 +1,18 @@
+import connection from '../models/connection';
+import OrderModel from '../models/order.model';
+import { IOrder } from '../interfaces';
+
+class OrderService {
+  public model: OrderModel;
+
+  constructor() {
+    this.model = new OrderModel(connection);
+  }
+
+  public async getAllOrders(): Promise<IOrder[]> {
+    const allOrders = this.model.getAllOrders();
+    return allOrders;
+  }
+}
+
+export default OrderService;
